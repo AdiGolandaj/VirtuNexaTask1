a8b7c885665b0f1c1359d9edf789f7b81d705797
Adi Golandaj <144472036+AdiGolandaj@users.noreply.github.com>
2025-03-15 16:06:15 +0530

    Update ContactsUs.jsx
    signup correction text edit

--- src/components/ContactsUs.jsx
+++ src/components/ContactsUs.jsx
@@ -31,7 +31,7 @@ const ContactsUs = () => {
         </div>
         <div className="lg:w-1/2 xl:w-2/3 flex flex-col justify-center items-center pl-9">
           <div className="flex font-mono font-extrabold justify-center items-center pr-20 text-white lg:text-4xl text-3xl pt-20">
-            <span className='font-mono'>S ignUp with us to Recieve the <span className='text-l2'>RIGHT</span> Treatment !</span>
+            <span className='font-mono'>SignUp with us to Recieve the <span className='text-l2'>RIGHT</span> Treatment !</span>
           </div>
           <div className='w-full mr-20 py-2 lg:scale-75 xl:scale-100'>
             <SignUpCard />
