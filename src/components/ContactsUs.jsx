44a410913b4b704a1e33442bdf6a284a60d734a0
internslucainvent <adigolandaj786@gmail.com>
2025-04-01 18:56:52 +0530

    updated to the latest github pages v4

--- src/components/ContactsUs.jsx
+++ src/components/ContactsUs.jsx
@@ -16,7 +16,7 @@ const ContactsUs = () => {
         </div>
 
         <div className="flex text-white w-screen text-xl justify-center text-center px-8">
-          <span>SignUp with us to Recieve the RIGHT Treatment !</span>
+          <span>Signup with us to Recieve the RIGHT Treatment !</span>
         </div>
 
         <div className="lg:hidden px-4 scale-60 md:-mt-0 -mt-20 md:scale-75">
@@ -31,7 +31,7 @@ const ContactsUs = () => {
         </div>
         <div className="lg:w-1/2 xl:w-2/3 flex flex-col justify-center items-center pl-9">
           <div className="flex font-mono font-extrabold justify-center items-center pr-20 text-white lg:text-4xl text-3xl pt-20">
-            <span className='font-mono'>S ignUp with us to Recieve the <span className='text-l2'>RIGHT</span> Treatment !</span>
+            <span className='font-mono'>SignUp with us to Recieve the <span className='text-l2'>RIGHT</span> Treatment !</span>
           </div>
           <div className='w-full mr-20 py-2 lg:scale-75 xl:scale-100'>
             <SignUpCard />
